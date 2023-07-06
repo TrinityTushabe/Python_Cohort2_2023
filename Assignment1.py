e2dbf59164f5b15039ca3db10e35fb48a470880c
TrinityTushabe <tushabetrinityfrances@gmail.com>
2023-07-06 05:58:30 +0300

    first commit

---
+++ Assignment1.py
@@ -0,0 +1,37 @@
+'''
+Introducining a conditional function with if loop function to display a workers Paye 
+The function prompts the workers to enter their year of birth, name , occupation and location
+And it prints the results if the eligbe to be taxed or not
+'''
+
+def paye(sal, name, age, occupation, location):
+    rate = 0.3
+    if sal >= 30000:
+        tax = rate * sal
+        netpay = sal - tax
+        print("****************************************************************")
+        print("Dear:", name)
+        print("Age:", age)
+        print("Occupation:", occupation)
+        print("Location:", location)
+        print("Your tax is payable:", tax)
+        print("And your take home salary is:", netpay)
+        print("................................................................")
+    else:
+        print("****************************************************************")
+        print("Dear:", name)
+        print("Age:", age)
+        print("Occupation:", occupation)
+        print("Location:", location)
+        print("Your salary is non-taxable")
+        print("................................................................")
+
+print("Welcome to our system!")  # welcome message
+name = raw_input("Please enter your name: ")  # prompting for the name
+birth_year = input("Please enter your year of birth: ")  # prompting for the year of birth
+occupation = raw_input("Please enter your occupation: ")  # prompting for the occupation
+location = raw_input("Please enter your location: ")  # prompting for the location
+age = 2023 - int(birth_year)  # calculate the age based on the birth year
+sal = float(raw_input("Please enter your salary: "))  # prompting for the salary
+
+paye(sal, name, age, occupation, location)
---
+++ README.md
@@ -0,0 +1,1 @@
+# Python_Cohort2_2023 Tushabe Trinity Francesco My first github Assignment in python
