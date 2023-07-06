1586362894c0544e82ce7c9edbc5c5118ba617bc
Winnie Among <w.among2023@gmail.com>
2023-07-06 18:45:36 +0300

    my first commit

---
+++ winnie2.py
@@ -0,0 +1,29 @@
+def register_pupils ():
+    pupils = [] #initialise an empty list to store pupils information
+    num_pupils = ("please enter the number of pupils you want to register: ")
+    print ("Your Welcome to Auntie Esther'Nursery School!!!")
+    amount = 400000
+    print("fees: ",amount)
+    name = raw_input("Please input your name here: ")
+    Date_Of_birth =raw_input("Please enter your date of birth: ")
+    Class = raw_input ("please enter your class here: ")
+    location =raw_input ("Please enter your location here: ")
+    parents_name =raw_input("please enter your parents name here: ")
+
+   
+
+
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
