d45339260f8edef4bcdf8175513c3f7c5cd69ade
WinnieAmong <136580580+WinnieAmong@users.noreply.github.com>
2023-07-11 20:51:17 +0300

    This is all about classes.

---
+++ winnie/OOP_Winnie.py
@@ -0,0 +1,385 @@
+class Fruit:
+    name="mango"
+    color="green"
+    shape="oval"
+print(Fruit.name)
+
+fruit2=Fruit()
+fruit2.name = "banana"
+fruit2.color = "yellow"
+fruit2.shape = "curved"
+print(f"{fruit2.name} is {fruit2.color} fruit")
+
+fruit3=Fruit()
+fruit3.name = "orange"
+fruit3.color = "orange"
+fruit3.shape = "circle"
+
+fruit4=Fruit()
+fruit4.name="pineapple"
+fruit4.color="cream"
+fruit4.shape="oval"
+
+
+fruit5=Fruit()
+fruit5.name="apple"
+fruit5.color="light green"
+fruit5.shape="circular"
+
+
+
+class School:
+    name="Dr.Obote College"
+    location="Lira"
+    region="Northern"
+
+print(School.name)
+
+school1=School()
+school1.name="Crane High school"
+school1.location="kitintale"
+school1.region="Central"
+print(f"{school1.name} is located in {school1.location} in the {school1.region} region")
+
+
+school2=School()
+school2.name="Mbale ss"
+school2.location="Mbale"
+school2.region="Eastern"
+
+
+school3=School()
+school3.name="St.Lawrence Nsonde"
+school3.location="Wakiso"
+school3.region="Central"
+
+
+
+school4=School()
+school4.name="Mandela SS"
+school4.location="Hoima"
+school4.region="Western"
+
+
+
+school5=School()
+school5.name="Kinyara Secondary school"
+school5.location="Masindi"
+school5.region="Western"
+
+class Furniture():
+    name="Chair"
+    color="blue"
+    shape="square"
+print(Furniture.name)
+
+furniture1=Furniture()
+
+furniture1.name="Table"
+furniture1.color="brown"
+furniture1.shape="rectangular"
+print(f"{furniture1.name} has {furniture1.shape} appearance")
+
+furniture2=Furniture()
+furniture2.name=""
+furniture2.color="brown"
+furniture2.shape="rectangle"
+
+furniture3=Furniture()
+furniture3.name="stool"
+furniture3.color="brown"
+furniture3.shape="circle"
+
+furniture4=Furniture()
+furniture4.name="door"
+furniture4.color="brown"
+furniture4.shape="rectangle"
+
+furniture5=Furniture()
+furniture5.name="window"
+furniture5.color="brown"
+furniture5.shape="rectangle"
+
+class Tribe():
+    name="Lango"
+    location="Oypam"
+    color="chocolate"
+print(Tribe.name)
+
+tribe1=Tribe()
+tribe1.name="Busoga"
+tribe1.location="Jinja"
+tribe1.color="Dark"
+print(f"{tribe1.name} is in {tribe1.location}")
+
+tribe2=Tribe()
+tribe2.name="Bunyankole"
+tribe2.location="Mbarara"
+tribe2.color="brown"
+
+tribe3=Tribe()
+tribe3.name="Acholi"
+tribe3.location="Gulu"
+tribe3.color="dark"
+
+tribe4=Tribe()
+tribe4.name="Baganda"
+tribe4.location="Masaka"
+tribe4.color="Dark"
+
+tribe5=Tribe()
+tribe5.name="Alur"
+tribe5.location="Nebbi"
+tribe5.color="Dark"
+
+
+class Bus():
+    name="YY"
+    color="blue"
+    route="mbale_to Kampala"
+print(Bus.name)
+
+
+bus1=Bus()
+bus1.name="link"
+bus1.color="green"
+bus1.route="masindi_kampala"
+print(f"{bus1.name} is {bus1.color}")
+
+
+bus2=Bus()
+bus2.name="Gaga"
+bus2.color="white"
+bus2.route="Kampala_lira"
+
+bus3=Bus()
+bus3.name="Kasumba coaches"
+bus3.color="yellow"
+bus3.route="kampala_Hoima"
+
+bus4=Bus()
+bus4.name=""
+bus4.color="green"
+bus4.route="masindi_kampala"
+
+bus5=Bus()
+bus5.name="link"
+bus5.color="green"
+bus5.route="masindi_kampala"
+
+
+class Mountain():
+    name="Elgon"
+    location="Uganda"
+
+
+mountain1=Mountain()
+mountain1.name="Kilimanjaro"
+mountain1.location="Tanzania"
+print(f"{mountain1.name} is found in {mountain1.location}")
+
+mountain2=Mountain()
+mountain2.name="Moroto"
+mountain2.location="Uganda"
+
+mountain3=Mountain()
+mountain3.name="Rwenzori"
+mountain3.location="Uganda"
+
+
+mountain4=Mountain()
+mountain4.name="Bisoke"
+mountain4.location="Rwanda"
+
+mountain5=Mountain()
+mountain5.name="Nyiragongo"
+mountain5.location="DRC"
+
+
+class Clothe():
+    name="dress"
+    color="red"
+    size="long"
+
+clothe1=Clothe
+clothe1.name="skirt"
+clothe1.color="blue"
+clothe1.size="short"
+print(f"The {clothe1.name} is {clothe1.color} ")
+
+clothe2=Clothe
+clothe2.name="sweater"
+clothe2.color="black"
+clothe2.size="large"
+
+
+clothe3=Clothe
+clothe3.name="t.shirt"
+clothe3.color="blue"
+clothe3.size="medium"
+
+clothe4=Clothe
+clothe4.name="trouser"
+clothe4.color="pink"
+clothe4.size="long"
+
+clothe5=Clothe
+clothe5.name="blouse"
+clothe5.color="purple"
+clothe5.size="medium"
+
+
+class University():
+    name="Makerere"
+    location="kampala"
+
+university1= University()
+university1.name="Gulu"
+university1.location="Gulu"
+print(f"{university1.name} is found in {university1.location} district")
+
+
+university2= University()
+university2.name="Muni"
+university2.location="Arua"
+
+
+university3= University()
+university3.name="IUIU"
+university3.location="Mbale"
+
+
+university4= University()
+university4.name="UCU"
+university4.location="Mukono"
+
+university5= University()
+university5.name="Mountains of the moon"
+university5.location="Fort portal"
+
+
+
+class Musician():
+    name="Levixon"
+    type_of_song="Gospel"
+print (Musician.name)
+
+musician1=Musician()
+musician1.name="Eddy kenzo"
+musician1.type_of_song="worldly"
+print(f"{musician1.name} is a {musician1.type_of_song} artist")
+    
+musician2=Musician()
+musician2.name="Grace Nakimera"
+musician2.type_of_song="gospel"
+
+musician3=Musician()
+mountain3.name="Maros"
+musician3.type_of_song="worldly"
+
+musician4=Musician()
+mountain4.name="Travis Green"
+musician4.type_of_song="gospel"
+
+musician5=Musician()
+musician5.name="Zuli tums"
+musician5.type_of_song="worldly"
+
+class Cereal():
+    name="maize"
+    life_span="Annual"
+
+    
+cereal1=Cereal()
+cereal1.name="wheat"
+cereal1.life_span="perenial"
+print(f"{cereal1.name} is a {cereal1.life_span} crop")
+
+
+cereal2=Cereal()
+cereal2.name="Rice"
+cereal2.life_span="perenial"
+print(f"{cereal2.name} is a {cereal2.life_span} crop")
+
+cereal3=Cereal()
+cereal3.name="millet"
+cereal3.life_span="Annual"
+
+
+cereal4=Cereal()
+cereal4.name="oats"
+cereal4.life_span="Annual"
+
+
+cereal5=Cereal()
+cereal5.name="barley"
+cereal5.life_span="Annual"
+
+class Disease():
+    name="Typhoid"
+    symptom="severe headache"
+
+
+disease1=Disease()
+disease1.name="malaria"
+disease1.symptom="vomiting"
+print(f"{disease1.name} is shown when {disease1.symptom} ")
+
+
+disease2=Disease()
+disease2.name="TB"
+disease2.symptom="Severe coughing"
+print(f"{disease2.name} is detected due to {disease2.symptom} ")
+
+
+disease3=Disease()
+disease3.name="Trachoma"
+disease3.symptom="itching of the eyes"
+
+
+disease4=Disease()
+disease4.name="Diarreal"
+disease4.symptom="running stomach"
+
+
+
+disease5=Disease()
+disease5.name="Asthma"
+disease5.symptom="short of breath"
+
+
+
+class King():
+    name="Ronald Mwenda"
+    tribe="Baganda"
+    district="Mpiggi"
+
+
+king1=King()
+king1.name="Iguru Gafabusa"
+king1.tribe="Bunyoro"
+king1.district="Hoima"
+print(f"{king1.name} is the {king1.tribe} king ")
+
+king2=King()
+king2.name="King Oyo"
+king2.tribe="Rutoro"
+king2.district="Fort Portal"
+
+
+king3=King()
+king3.name=""
+king3.tribe="Rutoro"
+king3.district="Fort Portal"
+
+
+    
+
+
+
+    
+
+
+    
+
+
